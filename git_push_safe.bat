a8b486129e0f9523ae07bf5c9f0e752b1f74565f
TasiYokan <guanghao.yang@outlook.com>
2017-12-27 02:04:12 -0500

    Now you can save local commit history on local_backup branch. (You are still working on local while local_backup is just for tracking)

--- git_push_safe.bat
+++ git_push_safe.bat
@@ -2,10 +2,15 @@
 
 REM Merge local to changes on master branch (if any) before push
 
+REM I don't know why return value will change to 0 if I put it at line 29...
+git rev-parse --verify --quite local_backup
+SET /A local_backup_exists=%ERRORLEVEL%
+
 REM Optional
 git checkout local
 git fetch origin master
 git rebase master
+
 IF %ERRORLEVEL% EQU 0 (
     ECHO [Merge local changes to master to push.]
     git checkout master
@@ -17,9 +22,24 @@ IF %ERRORLEVEL% EQU 0 (
     
     git push origin master
 
-    REM TODO: This will clean all local git history...
     git checkout local
-    git reset master@{0}
+    REM TODO: This will clean all local git history...
+    REM git reset master@{0}
+
+    ECHO %local_backup_exists%
+    IF %local_backup_exists% EQU 128 (
+        ECHO [local_backup doesn't exist! Simply rename local branch to local_backup and create a new one later.]
+        git branch -m local local_backup
+    ) ELSE (
+        ECHO [Rebaes local_backup success! Merge local to local_backup and delete local, finally create a new one later.]
+        git rebase local_backup
+        git checkout local_backup
+        git merge local
+        git branch -d local
+    )
+    git checkout master
+    git branch local
+    git checkout local
 ) ELSE (
     ECHO [Couldn't rebase due to changes on remote.]
     REM To let the merge happen on local
