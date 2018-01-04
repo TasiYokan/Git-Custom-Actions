ac0262516058e74ee7e9a7c2290da96db3b03772
TasiYokan <guanghao.yang@outlook.com>
2018-01-04 01:15:03 -0500

    Update local_backup history when we pull from orgion/master

--- git_pull_safe.bat
+++ git_pull_safe.bat
@@ -4,6 +4,12 @@ REM Updaet master branch then rebase local branch to latest version.
 REM Be sure the master branch is clean!"
 git checkout master
 git pull origin master
+git branch master_tmp
+git checkout master_tmp
+git rebase --onto local_backup local master_tmp
+git checkout local_backup
+git merge master_tmp
+git branch -d master_tmp
 git checkout local
 git rebase master
 IF %ERRORLEVEL% NEQ 0 (
